4db585003fac508ecd979fa31e7b8cbc4b0ce453
Caul Pearson <caulpearson@gmail.com>
2019-05-04 15:57:08 -0500

    Mars Rover page updated
    Now loads with a single picture.

--- src/app/marsrover/marsrover.component.ts
+++ src/app/marsrover/marsrover.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MarsroverComponent implements OnInit {
 
-  constructor() { }
+      constructor() { }
 
-  ngOnInit() {
+      ngOnInit() {
+        let start = new Date;
+        start.setDate(start.getDate() - 7);
+        let month = start.getMonth() + 1; //months from 1-12
+        let day = start.getDate();
+        let year = start.getFullYear();
+        
+
+    let api_key = 'GXFHdaG2tdwfk0BY8l9jvqzVIF58tUbzfy9y2h2k';
+
+    const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${year}-${month}-${day}&api_key=${api_key}`
+    fetch(url)
+
+  .then(response => {
+    if(response.status !== 200){
+      alert("Something went wrong. Please refresh the page");
+    }
+    //console.log(response.json());
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+    let container = document.getElementById('container');
+    let roverImg = data.photos[0].img_src;
+    let imgElement = document.createElement('img');
+    imgElement.src = roverImg;
+    container.appendChild(imgElement);
+  })
+
   }
 
 }
